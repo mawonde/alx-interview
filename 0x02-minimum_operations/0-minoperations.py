c97305ee65591deeec92be8bb96de3e421d3b602
mawonde <mawondep@gmail.com>
2023-12-14 19:42:57 +0000

    fix 4

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 """execute only two operations in this file:
 Copy All and Paste. Given a number n,
-write a method that calculates the fewest 
+write a method that calculates the fewest
 number of operations needed to result in
 exactly n H characters in the file."""
+
 
 def minOperations(n):
     """dp[i] represents the minimum operations
     needed to get i H characters"""
     if n < 2:
         return 0
-    list = []
-    i = 1
-    while n != 1:
-        i += 1
-        if n % i == 0:
-            while n % i == 0:
-                n /= i
-                list.append(i)
-    return sum(list)
+        list = []
+        i = 1
+        while n != 1:
+            i += 1
+            if n % i == 0:
+                while n % i == 0:
+                    n /= i
+                    list.append(i)
+        return sum(list)
