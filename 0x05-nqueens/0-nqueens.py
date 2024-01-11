b6d11a10e3b6bdb7e2adc5375c29b3ef0ab171f6
mawonde <mawondep@gmail.com>
2024-01-11 19:30:46 +0000

    fix 2

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -5,17 +5,17 @@ import sys
 
 
 def is_safe(board, row, col, N):
-    # Check if there is a queen in the same row
+    """Check if there is n q in row"""
     for i in range(col):
         if board[row][i] == 1:
             return False
 
-    # Check upper diagonal on left side
+    
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
 
-    # Check lower diagonal on left side
+    
     for i, j in zip(range(row, N, 1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
