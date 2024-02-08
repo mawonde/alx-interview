d0642e2e64b9c902595ff06dec695bff0b02c833
mawonde <mawondep@gmail.com>
2024-02-08 13:09:25 -0800

    Fix 2

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,25 +3,45 @@
 from typing import List
 
 
+def test0(n):
+    """
+    Returns: 0 if n is 1 or 1 if n is 0
+    """
+    if n == 0:
+        return 1
+    return 0
+
+
 def island_perimeter(grid: List[List[int]]) -> int:
     """
     Calculate the perimeter of the island described in grid.
 
     """
-    if not grid:
-        return 0
+    row = len(grid)
+    col = len(grid[0])
+    assert 1 <= row and col <= 100, "length must be between 1 an 100"
 
-    perimeter = 0
-    rows = len(grid)
-    cols = len(grid[0])
-
-    for i in range(rows):
-        for j in range(cols):
+    p = 0
+    for i in range(row):
+        for j in range(col):
+            assert (grid[i][j] == 0) or (grid[i][j] == 1), "grid numbers must be 0 or 1"
             if grid[i][j] == 1:
-                perimeter += 4
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
+                if i - 1 < 0:
+                    p += 1
+                else:
+                    p += test0(grid[i - 1][j])
+                if j - 1 < 0:
+                    p += 1
+                else:
+                    p += test0(grid[i][j - 1])
 
-    return perimeter
+                try:
+                    p += test0(grid[i + 1][j])
+                except IndexError:
+                    p += 1
+                try:
+                    p += test0(grid[i][j + 1])
+                except IndexError:
+                    p += 1
+
+    return p
